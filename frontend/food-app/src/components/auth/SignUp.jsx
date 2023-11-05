11935442ea00d73f6b3c6b3ea859cf536a442e27
Liu Martin <liumartin@s-164-67-208-177.resnet.ucla.edu>
2023-11-05 15:31:40 -0800

    Uncommented component

--- frontend/food-app/src/components/auth/SignUp.jsx
+++ frontend/food-app/src/components/auth/SignUp.jsx
@@ -2,24 +2,24 @@ import { useState } from "react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebase.js";
 
-export default function SignUpPrompt() {
+export default function SignUp() {
   const [email, setEmail] = useState("test@gmail.com");
   const [password, setPassword] = useState("testpass");
 
   const handleSignup = async () => {
-    // // First check that email and password are valid (meets length requirements, has special chars, etc.)
-    // try {
-    //   const userCredential = await createUserWithEmailAndPassword(
-    //     auth,
-    //     email,
-    //     password
-    //   );
-    //   // Handle successful signup
-    //   console.log("Signed up user:", userCredential.user);
-    // } catch (error) {
-    //   // Error handling
-    //   console.error("Error signing up:", error);
-    // }
+    // First check that email and password are valid (meets length requirements, has special chars, etc.)
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      // Handle successful signup
+      console.log("Signed up user:", userCredential.user);
+    } catch (error) {
+      // Error handling
+      console.error("Error signing up:", error);
+    }
   };
 
   return <button onClick={handleSignup}>Sign Up</button>;
