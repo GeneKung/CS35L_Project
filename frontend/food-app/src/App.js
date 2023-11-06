49a4fc80b5b127053e1a8cf2c4e97f6ae2d2060d
GeneKung <52974083+GeneKung@users.noreply.github.com>
2023-11-05 17:50:49 -0800

    Changed signIn and signUp to be split into two separate components

--- frontend/food-app/src/App.js
+++ frontend/food-app/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import SignIn from "./components/auth/SignIn";
 import RecipeGenerator from "./components/RecipeGenerator.jsx"
+import SignUp from "./components/auth/SignUp";
 
 function App() {
   return (
     <div className="App">
       <SignIn />
+      <SignUp />
       <RecipeGenerator />
     </div>
 
