861d6fd1c3fa209965f7452c5c1c8008841f0b53
rizvi-ha <136656057+rizvi-ha@users.noreply.github.com>
2023-11-15 18:32:24 -0800

    Adding allergy and dietary restriction support to generateRecipe
    Had to make the function take in a few extra lists as input. Also
    removed maxTokens param bc the automatic behavior is better than
    computing the tokens manually

--- frontend/food-app/src/components/RecipeGenerator.jsx
+++ frontend/food-app/src/components/RecipeGenerator.jsx
@@ -3,13 +3,15 @@ import generateRecipe from "../generateRecipe"
 
 export default function RecipeGenerator() {
   const [ingredients, setIngredients] = useState('');
+  const [dietary, setDietary] = useState('');
+  const [allergies, setAllergies] = useState('');
   const [recipe, setRecipe] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
   const handleGenerateClick = async () => {
     setIsLoading(true);
     try {
-      const generatedRecipe = await generateRecipe(ingredients);
+      const generatedRecipe = await generateRecipe(ingredients, dietary, allergy);
       setRecipe(generatedRecipe);
     } catch (error) {
       console.error("Failed to generate recipe:", error);
@@ -26,6 +28,24 @@ export default function RecipeGenerator() {
         name="ingredients"
         onChange={(e) => setIngredients(e.target.value)}
         placeholder="Enter ingredients, separated by commas"
+        rows={5}
+        style={{ width: '100%' }}
+      />
+      <textarea
+        value={dietary}
+        id="dietaryInput"
+        name="dietary"
+        onChange={(e) => setDietary(e.target.value)}
+        placeholder="Enter dietary, separated by commas"
+        rows={5}
+        style={{ width: '100%' }}
+      />
+      <textarea
+        value={allergies}
+        id="allergiesInput"
+        name="allergies"
+        onChange={(e) => setAllergies(e.target.value)}
+        placeholder="Enter allergies, separated by commas"
         rows={5}
         style={{ width: '100%' }}
       />
