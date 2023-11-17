2d1b5d50ad7b90286450480d3d594ef113dc6707
rizvi-ha <136656057+rizvi-ha@users.noreply.github.com>
2023-11-17 15:07:40 -0800

    Updated RecipeGenerator page to work standalone
    Made it an extended version of the Fridge page in structure

--- frontend/food-app/src/components/RecipeGenerator.jsx
+++ frontend/food-app/src/components/RecipeGenerator.jsx
@@ -1,18 +1,21 @@
-/*
+
 import { useState } from "react";
 import generateRecipe from "../generateRecipe"
 
 export default function RecipeGenerator() {
-  const [ingredients, setIngredients] = useState('');
-  const [dietary, setDietary] = useState('');
-  const [allergies, setAllergies] = useState('');
+  const [ingr, setIngr] = useState([]);
+  const [allergies, setAllergies] = useState([]);
+  const [dietary, setDietary] = useState([]);
   const [recipe, setRecipe] = useState('');
   const [isLoading, setIsLoading] = useState(false);
-
+  const [inputValue1, setInputValue1] = useState("");
+  const [inputValue2, setInputValue2] = useState("");
+  const [inputValue3, setInputValue3] = useState("");
+  
   const handleGenerateClick = async () => {
     setIsLoading(true);
     try {
-      const generatedRecipe = await generateRecipe(ingredients, dietary, allergy);
+      const generatedRecipe = await generateRecipe(ingr, dietary, allergies);
       setRecipe(generatedRecipe);
     } catch (error) {
       console.error("Failed to generate recipe:", error);
@@ -20,40 +23,78 @@ export default function RecipeGenerator() {
     setIsLoading(false);
   };
 
+  function addIngredient(e){
+    e.preventDefault();
+    if (inputValue1 === "") return;
+    setIngr((prev) => [...prev, inputValue1]);
+    setInputValue1(""); // Clear input value after adding
+  }
+
+  function addDietary(e){
+    e.preventDefault();
+    if (inputValue3 === "") return;
+    setDietary((prev) => [...prev, inputValue3]);
+    setInputValue3(""); // Clear input value after adding
+  }
+
+  function addAllergy(e){
+    e.preventDefault();
+    if (inputValue2 === "") return;
+    setAllergies((prev) => [...prev, inputValue2]);
+    setInputValue2(""); // Clear input value after adding
+  }
+  
   return (
-    <div>
-      <h1>Recipe Generator</h1>
-      <textarea
-        value={ingredients}
-        id="ingredientsInput"
-        name="ingredients"
-        onChange={(e) => setIngredients(e.target.value)}
-        placeholder="Enter ingredients, separated by commas"
-        rows={5}
-        style={{ width: '100%' }}
-      />
-      <textarea
-        value={dietary}
-        id="dietaryInput"
-        name="dietary"
-        onChange={(e) => setDietary(e.target.value)}
-        placeholder="Enter dietary, separated by commas"
-        rows={5}
-        style={{ width: '100%' }}
-      />
-      <textarea
-        value={allergies}
-        id="allergiesInput"
-        name="allergies"
-        onChange={(e) => setAllergies(e.target.value)}
-        placeholder="Enter allergies, separated by commas"
-        rows={5}
-        style={{ width: '100%' }}
-      />
-      <button onClick={handleGenerateClick} disabled={isLoading}>
+    <>
+    <form onSubmit={addIngredient}>
+        <label>
+          New Ingredient:
+          <input
+            type="text"
+            value={inputValue1}
+            onChange={(e) => setInputValue1(e.target.value)}
+          />
+        </label>
+        <button type="submit">Add</button>
+    </form>
+    <form onSubmit={addAllergy}>
+        <label>
+          New Allergy:
+          <input
+            type="text"
+            value={inputValue2}
+            onChange={(e) => setInputValue2(e.target.value)}
+          />
+        </label>
+        <button type="submit">Add</button>
+    </form>
+    <form onSubmit={addDietary}>
+        <label>
+          New Dietary Restriction:
+          <input
+            type="text"
+            value={inputValue3}
+            onChange={(e) => setInputValue3(e.target.value)}
+          />
+        </label>
+        <button type="submit">Add</button>
+    </form>
+    <button onClick={handleGenerateClick} disabled={isLoading}>
         {isLoading ? 'Generating...' : 'Generate Recipe'}
-      </button>
-      <div>
+    </button>
+    <h3>Ingredients:</h3>
+    {ingr.map(item1 =>( 
+      <div>{item1}</div>
+    ))}
+    <h3>Allergies:</h3>
+    {allergies.map(item2 =>( 
+      <div>{item2}</div>
+    ))}
+    <h3>Dietary Restrictions:</h3>
+    {dietary.map(item3 =>( 
+      <div>{item3}</div>
+    ))}
+    <div>
         {recipe && (
           <>
             <h2>Generated Recipe</h2>
@@ -61,7 +102,6 @@ export default function RecipeGenerator() {
           </>
         )}
       </div>
-    </div>
-  );
+    </>
+  )
 };
-*/
