ae9716f92cfc10639cfd63235b0f61a5a920f3a3
rizvi-ha <136656057+rizvi-ha@users.noreply.github.com>
2023-11-17 15:24:33 -0800

    Updated Home.jsx to better reflect site structure
    We can edit the visuals/exact wording later but this makes for
    a more logical homepage I think

--- frontend/food-app/src/components/display/Home.jsx
+++ frontend/food-app/src/components/display/Home.jsx
@@ -5,24 +5,25 @@ export default function Home() {
   return (
     <>
       <h1>Generate Recipes with AI</h1>
+      <p> Welcome to Food-App! This is a website where you can generate recipes based on the ingredients you have available, as well as your dietary restrictions and allergies!</p>
       <div className="container">
         <div className="grid">
           {/* <Link to="/ingredients" style={{ textDecoration: "none" }}> */}
           <div className="card">
-            <h2>Add Ingredients</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <h2>Fridge</h2>
+            <p>Visit your fridge, where you can keep a log of the ingredients you have available</p>
           </div>
           {/* </Link> */}
           <Link to="/generate" style={{ textDecoration: "none" }}>
             <div className="card">
               <h2>Generate Recipes</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <p>Generate a recipe from scratch by entering ingredients and other details</p>
             </div>
           </Link>
           <Link to="/saved" style={{ textDecoration: "none" }}>
             <div className="card">
-              <h2>Save Recipes</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <h2>Saved Recipes</h2>
+              <p>View previous recipes that you saved for later!</p>
             </div>
           </Link>
         </div>
