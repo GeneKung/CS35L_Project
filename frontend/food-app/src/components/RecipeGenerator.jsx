02d7a2855be3a9b25a4174c4f0a63820b03e11fb
Liu Martin <liumartin@Lius-MacBook-Pro.local>
2023-11-28 20:35:20 -0800

    Made changes to Fridge.jsx for fetching/updating firestore fridge doc

--- frontend/food-app/src/components/RecipeGenerator.jsx
+++ frontend/food-app/src/components/RecipeGenerator.jsx
@@ -1,18 +1,17 @@
-
 import { useState } from "react";
 import generateRecipe from "../generateRecipe";
-import ReactMarkdown from 'react-markdown';
+import ReactMarkdown from "react-markdown";
 
 export default function RecipeGenerator() {
   const [ingr, setIngr] = useState([]);
   const [allergies, setAllergies] = useState([]);
   const [dietary, setDietary] = useState([]);
-  const [recipe, setRecipe] = useState('');
+  const [recipe, setRecipe] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [inputValue1, setInputValue1] = useState("");
   const [inputValue2, setInputValue2] = useState("");
   const [inputValue3, setInputValue3] = useState("");
-  
+
   const handleGenerateClick = async () => {
     setIsLoading(true);
     try {
@@ -24,30 +23,30 @@ export default function RecipeGenerator() {
     setIsLoading(false);
   };
 
-  function addIngredient(e){
+  function addIngredient(e) {
     e.preventDefault();
     if (inputValue1 === "") return;
     setIngr((prev) => [...prev, inputValue1]);
     setInputValue1(""); // Clear input value after adding
   }
 
-  function addDietary(e){
+  function addDietary(e) {
     e.preventDefault();
     if (inputValue3 === "") return;
     setDietary((prev) => [...prev, inputValue3]);
     setInputValue3(""); // Clear input value after adding
   }
 
-  function addAllergy(e){
+  function addAllergy(e) {
     e.preventDefault();
     if (inputValue2 === "") return;
     setAllergies((prev) => [...prev, inputValue2]);
     setInputValue2(""); // Clear input value after adding
   }
-  
+
   return (
     <>
-    <form onSubmit={addIngredient}>
+      <form onSubmit={addIngredient}>
         <label>
           New Ingredient:
           <input
@@ -57,8 +56,8 @@ export default function RecipeGenerator() {
           />
         </label>
         <button type="submit">Add</button>
-    </form>
-    <form onSubmit={addAllergy}>
+      </form>
+      <form onSubmit={addAllergy}>
         <label>
           New Allergy:
           <input
@@ -68,8 +67,8 @@ export default function RecipeGenerator() {
           />
         </label>
         <button type="submit">Add</button>
-    </form>
-    <form onSubmit={addDietary}>
+      </form>
+      <form onSubmit={addDietary}>
         <label>
           New Dietary Restriction:
           <input
@@ -79,23 +78,23 @@ export default function RecipeGenerator() {
           />
         </label>
         <button type="submit">Add</button>
-    </form>
-    <button onClick={handleGenerateClick} disabled={isLoading}>
-        {isLoading ? 'Generating...' : 'Generate Recipe'}
-    </button>
-    <h3>Ingredients:</h3>
-    {ingr.map(item1 =>( 
-      <div>{item1}</div>
-    ))}
-    <h3>Allergies:</h3>
-    {allergies.map(item2 =>( 
-      <div>{item2}</div>
-    ))}
-    <h3>Dietary Restrictions:</h3>
-    {dietary.map(item3 =>( 
-      <div>{item3}</div>
-    ))}
-    <div>
+      </form>
+      <button onClick={handleGenerateClick} disabled={isLoading}>
+        {isLoading ? "Generating..." : "Generate Recipe"}
+      </button>
+      <h3>Ingredients:</h3>
+      {ingr.map((item1) => (
+        <div>{item1}</div>
+      ))}
+      <h3>Allergies:</h3>
+      {allergies.map((item2) => (
+        <div>{item2}</div>
+      ))}
+      <h3>Dietary Restrictions:</h3>
+      {dietary.map((item3) => (
+        <div>{item3}</div>
+      ))}
+      <div>
         {recipe && (
           <>
             <h2>Generated Recipe</h2>
@@ -104,7 +103,5 @@ export default function RecipeGenerator() {
         )}
       </div>
     </>
-
-    
-  )
-};
+  );
+}
--- frontend/food-app/src/components/display/Fridge.jsx
+++ frontend/food-app/src/components/display/Fridge.jsx
@@ -1,34 +1,87 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { doc, updateDoc, getDoc } from "firebase/firestore";
+import { db } from "../../firebase.js";
+import { useAuth } from "../auth/AuthContext";
 import generateRecipe from "../../generateRecipe";
-import ReactMarkdown from 'react-markdown';
+import ReactMarkdown from "react-markdown";
 
 function Fridge(props) {
   const [ingr, setIngr] = useState([]);
   const [inputValue1, setInputValue1] = useState("");
-  const [recipe, setRecipe] = useState('');
+  const [recipe, setRecipe] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const currentUser = useAuth();
 
-  function addIngredient(e){
+  useEffect(() => {
+    const getFridgeData = async () => {
+      if (currentUser) {
+        const userDocRef = doc(db, "users", currentUser.uid);
+        const userDocSnap = await getDoc(userDocRef);
+
+        if (userDocSnap.exists()) {
+          const userData = userDocSnap.data();
+          const fridgeId = userData.fridgeId;
+
+          const fridgeDocRef = doc(db, "fridges", fridgeId);
+          const fridgeDocSnap = await getDoc(fridgeDocRef);
+
+          if (fridgeDocSnap.exists()) {
+            const fridgeData = fridgeDocSnap.data();
+            setIngr(fridgeData.ingredients || []); // Assuming ingredients are stored in 'ingredients'
+          }
+        }
+      }
+    };
+
+    getFridgeData();
+  }, [currentUser]);
+
+  async function updateFridgeInFirestore(newIngredientList) {
+    if (currentUser) {
+      const userDocRef = doc(db, "users", currentUser.uid);
+      const userDocSnap = await getDoc(userDocRef);
+
+      if (userDocSnap.exists()) {
+        const userData = userDocSnap.data();
+        const fridgeId = userData.fridgeId;
+
+        const fridgeDocRef = doc(db, "fridges", fridgeId);
+        await updateDoc(fridgeDocRef, { ingredients: newIngredientList });
+
+        if (fridgeDocSnap.exists()) {
+          const fridgeData = fridgeDocSnap.data();
+          setIngr(fridgeData.ingredients || []); // Assuming ingredients are stored in 'ingredients'
+        }
+      }
+    }
+  }
+
+  function addIngredient(e) {
     e.preventDefault();
     if (inputValue1 === "") return;
     setIngr((prev) => [...prev, inputValue1]);
+    updateFridgeInFirestore(ingr);
     setInputValue1(""); // Clear input value after adding
   }
 
   const handleGenerateClick = async () => {
     setIsLoading(true);
     try {
-      const generatedRecipe = await generateRecipe(ingr, props.diet, props.allergy);
+      const generatedRecipe = await generateRecipe(
+        ingr,
+        props.diet,
+        props.allergy
+      );
       setRecipe(generatedRecipe);
     } catch (error) {
       console.error("Failed to generate recipe:", error);
     }
     setIsLoading(false);
   };
-  
+
   return (
     <>
-    <form onSubmit={addIngredient}>
+      <form onSubmit={addIngredient}>
         <label>
           New Ingredient:
           <input
@@ -38,15 +91,15 @@ function Fridge(props) {
           />
         </label>
         <button type="submit">Add</button>
-    </form>
-    <button onClick={handleGenerateClick} disabled={isLoading}>
-        {isLoading ? 'Generating...' : 'Generate Recipe'}
-    </button>
-    <h3>Saved Ingredients:</h3>
-    {ingr.map(item1 =>( 
-      <div>{item1}</div>
-    ))}
-    <div>
+      </form>
+      <button onClick={handleGenerateClick} disabled={isLoading}>
+        {isLoading ? "Generating..." : "Generate Recipe"}
+      </button>
+      <h3>Saved Ingredients:</h3>
+      {ingr.map((item1) => (
+        <div>{item1}</div>
+      ))}
+      <div>
         {recipe && (
           <>
             <h2>Generated Recipe</h2>
@@ -55,7 +108,7 @@ function Fridge(props) {
         )}
       </div>
     </>
-  )
+  );
 }
 
 export default Fridge;
