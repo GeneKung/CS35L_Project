69e7a5dd947d56badde845273cc83ea8db7fc0e3
GeneKung <52974083+GeneKung@users.noreply.github.com>
2023-12-03 15:27:18 -0800

    cleaned up SearchRecipes

--- frontend/food-app/src/components/display/SearchRecipes.jsx
+++ frontend/food-app/src/components/display/SearchRecipes.jsx
@@ -1,9 +1,7 @@
 import "./SearchRecipes.css";
 import React, { useEffect, useState } from 'react';
-import getAllRecipes from "../../database/recipes";
 import ReactMarkdown from 'react-markdown';
 import * as emoji from 'node-emoji'
-
 
 function generateTitleEmojis(recipeTitle){
   let words = recipeTitle.split(" ");
@@ -18,24 +16,13 @@ function generateTitleEmojis(recipeTitle){
   return recipeTitle;
 }
 
-export default function SearchRecipes() {
-
+export default function SearchRecipes( {inputData} ) {
+  
   return (
-    <body>
-      <header className="saved-header">
-        <h1 style={{fontSize: "50px", textShadow: "5px 5px 10px rgba(0,0,0,0.5)"}}>Search Recipes</h1>
-      </header>
-      <div className="saved-search-bar">
-        <p style={{fontSize:"15px"}}>Search for Recipes: </p>
+    <div className="container">
+      <h1>Hi</h1>
+    </div>
 
-        <intput type="text" placeholder="Type a recipe name" style={{width: "50%"}}/>
-      </div>
-      <div className="container">
-        
-            
-      </div>
-    </body>
   );
 
 }
-
